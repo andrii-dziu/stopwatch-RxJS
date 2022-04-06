89909052cd6f96f26e1bf5d24ca25a26dad8e2f6
Andrii <dzura1996das@gmail.com>
2022-04-06 14:23:48 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+import { Subscription, fromEvent, interval } from 'rxjs';
+import { buffer, filter, map } from "rxjs/operators";
+import * as moment from "moment"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'stopwatchRxJS';
+export class AppComponent implements OnDestroy, AfterViewInit {
+  timer: any;
+  seconds: number = 0;
+
+  @ViewChild('wait', { static: true }) wait!: ElementRef;
+  btnWait!: Subscription;
+  isEnable: boolean = false;
+  display: number = 0;
+  ngAfterViewInit() {
+    this.btnWait = fromEvent(this.wait.nativeElement, 'click').
+    pipe(
+      buffer(interval(300)),
+      filter((clicks) => clicks.length === 2 )
+    ).subscribe(()=> {
+      this.timer.unsubscribe();
+      this.isEnable= false
+    })
+  }
+
+  ngOnDestroy() {
+    this.btnWait.unsubscribe()
+    this.timer.unsubscribe()
+  }
+
+  onStart() {
+    this.isEnable = true;
+    this.timer = interval(1000)
+    .pipe(map(() => {
+      this.seconds++
+    })
+    ).subscribe(() => this.display = this.seconds)
+  }
+  onStop() {
+    if (this.timer) {
+      this.timer.unsubscribe();
+      this.resetCounter();
+      this.isEnable = false;
+    }
+  }
+
+  resetCounter() {
+    this.seconds = 0;
+  }
+
+  onReset() {
+    this.timer.unsubscribe();
+    this.resetCounter();
+    this.onStart();
+  }
+
+  formatTime(value: number): string {
+    return moment().startOf("day").seconds(value).format("HH:mm:ss");
+  }
 }
+
+
+
